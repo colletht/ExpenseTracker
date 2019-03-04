8a0f5273d078e3ed4568e65a01b0a8e7cdd1d9e8
Harry Collet <harrison.collet@wsu.edu>
2019-03-04 13:28:01 -0800

    Created file for journal class

---
+++ Journal.py
@@ -0,0 +1,8 @@
+import DigitalReciept
+
+#journal will work similar to accounting journal, keep track of expenses and entries to different categories. Contains functions to get analytics from spendings
+class Journal:
+    def __init__(self):
+        self.journal = []
+
+    
