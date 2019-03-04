b70acfebfafff6516a375240bf967d2742fca967
Harry Collet <harrison.collet@wsu.edu>
2019-03-04 11:37:31 -0800

    Added toString for DigitalReciept class Modularized query functions for member variables added editing funcitonality

--- DigitalReciept.py
+++ DigitalReciept.py
@@ -3,21 +3,41 @@ from datetime import date
 #File contains definition for Digital Reciept object that will store data for an individual reciept
 
 #digital reciept class, holds data for reciept
-#Member variables: cost, cardNum, dateOfPurchase, placeOfPurchase
 class DigitalReciept:
     def __init__(self):
         self.cost = 0.0
+        self.signCost = -1
         self.cardNum = ""
         self.dateOfPurchase = date.today()
         self.placeOfPurchase = ""
         self.information = ""
         self.genre = ""
-        self.genres = ['Food','Hygene','Cleaning','Clothes','Alcohol','Recreation']
+        self.genres = ['Food','Hygene','Cleaning','Clothes','Alcohol','Recreation','Gaming']
 
+    #getters
     def getDate(self):
         return self.dateOfPurchase
 
-    def readDate(self):
+    def getCost(self):
+        return self.cost
+
+    def getRealCost(self):
+        return self.cost*self.signCost
+
+    def getCardNum(self):
+        return self.cardNum
+    
+    def getPlaceOfPurchase(self):
+        return self.placeOfPurchase
+
+    def getGenre(self):
+        return self.genre
+    
+    def getInfo(self):
+        return self.information
+
+    #query functions defined here: each function queries an attribute from the terminal user, modulated into functions to make future error checking easier
+    def queryDate(self):
         success = False
         while success == False:
             try:
@@ -27,13 +47,21 @@ class DigitalReciept:
                 success = True
             except ValueError:
                 print("Please make sure your date is valid and entered in US format.")
-            
-        
-    '''
-    def writeToCSV(self):
-        #write all data here to file
-        if 
-    '''
+
+    def queryCardNum(self):
+        self.cardNum = input("Enter four digit card number:\t")
+
+    def queryCost(self):
+        sign = input("Debit or Credit to account:\t").lower()
+
+        while sign != "debit" and sign != "credit":
+            sign = input("Debit or Credit to account:\t").lower()
+        if sign == "debit":
+            self.signCost = 1
+        else:
+            self.signCost = -1
+
+        self.cost = float(input("Enter the cost accrued:\t\t$"))
 
     def queryGenre(self):
         i = 1
@@ -41,30 +69,59 @@ class DigitalReciept:
             print(i, x, sep = '\t', end = '\n')
             i+=1
         self.genre = self.genres[int(input("Select a genre by number:\t"))-1]
+    
+    def queryPlace(self):
+        self.placeOfPurchase = input("Enter location of purchase:\t")
+
+    def queryInfo(self):
+        self.information = input("Enter any extra information:\t")
+
+    #edits the member data of a Digital Reciept Object, allows for user choice of which fields to edit
+    def editReciept(self):
+        option = 0
+        while option is not 7:
+            while option < 1 and option > 7:
+                print(self.toString())
+                print("What field do you wish to edit:",
+                    "1. Date of Purchase",
+                    "2. Card Number",
+                    "3. Cost",
+                    "4. Genre",
+                    "5. Place of Purchase",
+                    "6. Notes"
+                    "7. Done", sep = '\n',)
+                option = input(">> ")
+            if option == 1:
+                self.queryDate()
+            elif option == 2:
+                self.queryCardNum()
+            elif option == 3:
+                self.queryCost()
+            elif option == 4:
+                self.queryGenre()
+            elif option == 5:
+                self.queryPlace()
+            else:
+                self.queryInfo()
 
     #fills the reciept in case any missing data was not provided in the constructor
     def fillReciept(self):
-        self.placeOfPurchase = input("Enter location of purchase:\t")
+        self.queryPlace()
 
-        self.readDate()
+        self.queryDate()
 
-        self.cardNum = input("Enter four digit card number:\t")
+        self.queryCardNum()
 
-        sign = input("Debit or Credit:\t\t").lower()
-        while sign != "debit" and sign != "credit":
-            sign = input("Debit or Credit:\t\t").lower()
-
-        self.cost = float(input("Enter the cost accrued:\t\t"))
-        if sign == "debit":
-            self.cost *= -1
+        self.queryCost()
 
         self.queryGenre()
         
-        self.information = input("Enter any extra information:\t")
+        self.queryInfo()
 
+    #parses member data into a formatted string
     def toString(self):
-        print(self.dateOfPurchase,self.cardNum, '$' + str(abs(self.cost)), self.genre, self.placeOfPurchase, self.information, sep = '\t', end = '\n')
+        return "{0:<9.8} {1:<4}  ${2:>8.2f}  {4:^12.11}  {3:^14.13}  Notes:{5:<50}".format(self.dateOfPurchase.strftime("%x"), self.cardNum, self.cost*self.signCost, self.placeOfPurchase, self.genre, self.information)
 
 y = DigitalReciept()
 y.fillReciept()
-y.toString()
+print(y.toString())
