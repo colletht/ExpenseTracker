b79ebc21fa55bbebe3a3ccccd51854d1e7a03a70
Harry Collet <harrison.collet@wsu.edu>
2019-03-27 12:25:08 -0700

    Added new filter class for filtering reciepts to DigitalReciept.py and added a few functionalities to Journal.py

--- DigitalReciept.py
+++ DigitalReciept.py
@@ -46,6 +46,9 @@ class DigitalReciept:
     def getCost(self):
         return self.cost
 
+    def getSign(self):
+        return self.signCost
+
     def getRealCost(self):
         return self.cost*self.signCost
 
@@ -105,7 +108,8 @@ class DigitalReciept:
     def editReciept(self, genres):
         option = 0
         while option is not 7:
-            while option < 1 and option > 7:
+            option = 0
+            while option < 1 or option > 7:
                 print(self)
                 print("What field do you wish to edit:",
                     "1. Date of Purchase",
@@ -113,9 +117,9 @@ class DigitalReciept:
                     "3. Cost",
                     "4. Genre",
                     "5. Place of Purchase",
-                    "6. Notes"
+                    "6. Notes",
                     "7. Done", sep = '\n',)
-                option = input(">> ")
+                option = int(input(">> "))
             if option == 1:
                 self.queryDate()
             elif option == 2:
@@ -142,3 +146,139 @@ class DigitalReciept:
         self.queryGenre(genres)
         
         self.queryInfo()
+
+class FilterReciept:
+    def __init__(self):
+        self.loCost = None
+        self.hiCost = None
+        self.signCost = None
+        self.cardNum = None
+        self.startDate = None
+        self.endDate = None
+        self.placeOfPurchase = None
+        self.genre = None
+        self.keyWord = None
+    
+    def match(self, reciept):
+        matches = True
+        if self.startDate is not None:
+            matches = matches and self.startDate <= reciept.getDate()
+        if self.endDate is not None:
+             matches = matches and self.endDate > reciept.getDate()
+        if self.cardNum is not None:
+            matches = matches and (self.cardNum == reciept.getCardNum())
+        if self.signCost is not None:
+            matches = matches and (self.signCost == reciept.getSign())
+        if self.loCost is not None:
+            matches = matches and (self.loCost <= reciept.getCost())
+        if self.hiCost is not None:
+            matches = matches and (self.hiCost > reciept.getCost())
+        if self.placeOfPurchase is not None:
+            matches = matches and (self.placeOfPurchase == reciept.getPlaceOfPurchase())
+        if self.genre is not None:
+            matches = matches and (self.genre == reciept.getGenre())
+        if self.keyWord is not None:
+            matches = matches and (self.keyWord in reciept.getInfo())
+        return matches
+
+    def queryCost(self):
+        x = input("Filter by Debit/Credit (Enter for none)? (D/C)\t")
+        while x is not "D" and x is not "C" and x is not "N":
+            x = input("Filter by Debit/Credit (Enter for none)? (D/C)\t")
+        if x is "D":
+            self.signCost = 1
+        elif x is "C":
+            self.signCost = -1
+            
+        x = input("Enter low range value (Press enter for none):\t")
+        if x is not '':
+            self.loCost = float(x)
+        x = input("Enter high range value (Press enter for none):\t")
+        if x is not '':
+            self.hiCost = float(x)
+    
+    def queryCardNum(self):
+        self.cardNum = input("Enter 4 digit Card Number:\t")
+    
+    def queryDate(self):
+        success = False
+        while success == False:
+            try:
+                dateStr = input("Enter the date for low range (Press Enter for none):\t")
+                     
+                if dateStr is "":
+                    success = True
+                    break
+
+                patt = re.search(r'(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2,4})',dateStr)
+                self.startDate = date(int(patt.group(3)), int(patt.group(1)), int(patt.group(2)))
+                success = True
+            except ValueError:
+                print("Please make sure your date is valid and entered in US format.")
+            
+        success = False
+        while success == False:
+            try:
+                dateStr = input("Enter the date for high range (Press Enter for none):\t")
+
+                if dateStr is "":
+                    success = True
+                    break
+
+                patt = re.search(r'(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2,4})',dateStr)
+                self.endDate = date(int(patt.group(3)), int(patt.group(1)), int(patt.group(2)))
+                success = True
+            except ValueError:
+                print("Please make sure your date is valid and entered in US format.")
+
+    def queryPlace(self):
+        self.placeOfPurchase = input("Enter Place of Purchase: \t")
+        
+    def queryGenre(self, genres):
+        i = 1
+        for x in genres:
+            print(i, x, sep = '\t', end = '\n')
+            i+=1
+        self.genre = genres[int(input("Select a genre by number:\t"))-1]
+
+    def queryKeyword(self):
+        self.keyWord = input("Enter keyword:\t\t")
+
+    def fillFilter(self, genres):
+        option = 0
+        while option is not 7:
+            option = 0
+            while option < 1 or option > 7:
+                print("What fields would you like to filter by:",
+                    "1. Date of Purchase",
+                    "2. Card Number",
+                    "3. Cost",
+                    "4. Genre",
+                    "5. Place of Purchase",
+                    "6. Keyword",
+                    "7. Done", sep = '\n',)
+                option = int(input(">> "))
+
+            if option == 1:
+                self.queryDate()
+            elif option == 2:
+                self.queryCardNum()
+            elif option == 3:
+                self.queryCost()
+            elif option == 4:
+                self.queryGenre(genres)
+            elif option == 5:
+                self.queryPlace()
+            elif option == 6:
+                self.queryKeyword()
+
+        
+
+r = DigitalReciept()
+r.fillReciept(['Food','Hygene','Cleaning','Clothes','Alcohol','Recreation','Gaming'])
+print(r)
+f = FilterReciept()
+f.fillFilter(['Food','Hygene','Cleaning','Clothes','Alcohol','Recreation','Gaming'])
+print(f.match(r))
+
+#TODO: make all query functions error proof so code does not break if incorrect input is entered
--- Journal.py
+++ Journal.py
@@ -1,4 +1,4 @@
-import DigitalReciept
+import DigitalReciept, functools
 
 #journal will work similar to accounting journal, keep track of expenses and entries to different categories. Contains functions to get analytics from spendings
 class Journal:
@@ -6,5 +6,20 @@ class Journal:
         self.journal = []
         self.budget = 0
         self.genres = ['Food','Hygene','Cleaning','Clothes','Alcohol','Recreation','Gaming']
-
+        self.curFilter = None   
+        self.filters = {}    #will hold dictionary of saved filters and their names, filters are FilterReciept objects with special values to be evauluated by the matches filter function
     
+    def addReciept(self):
+        p = DigitalReciept.DigitalReciept()
+        p.fillReciept(self.genres)
+        self.journal += p
+        self.journal.sort()
+        #TODO: inefficient: figure out an insert in order way rather than needlessly sorting list every time
+
+    def editReciept(self, reciept):
+        reciept.editReciept(self.genres)
+
+    def applyFilter(self):
+        subJournal = filter(self.curFilter.match, self.journal)
+        return subJournal
+
